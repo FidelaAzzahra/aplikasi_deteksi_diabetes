35f87af1379019df800d9196912c6a483c38da99
Fidela Azzahra <114632917+FidelaAzzahra@users.noreply.github.com>
2023-06-24 15:20:34 +0700

    prediksi_diabetes.py

--- prediksi_diabetes.py
+++ prediksi_diabetes.py
@@ -1,89 +1,102 @@
-# Aziza Azka Sajida
-# Fidela Azzahra
-# Salsabila Ayu Anjelina
-# Salma Shafira Fatya Ardyani
-
 import streamlit as st # library untuk import streamlit nya
 import pandas as pd # library untuk membaca dataset dan analisis datanya
 import seaborn as sns # library untuk visualisasi data dan membuat heatmap
 import matplotlib.pyplot as plt # library untuk visualisasi data
 import matplotlib.ticker as ticker # library untuk mengatur penanda sumbu plot
+
 from sklearn.neighbors import KNeighborsClassifier # library scikit-learn yang digunakan untuk mengimplementasikan algoritma K-Nearest Neighbors (KNN)
 from sklearn.model_selection import train_test_split # fungsi dalam library scikit-learn yang digunakan untuk membagi dataset menjadi subset train dan test secara acak
 from sklearn.preprocessing import StandardScaler # kelas dalam library scikit-learn yang digunakan untuk melakukan penskalaan fitur pada dataset
+from sklearn.metrics import accuracy_score #  metrics digunakan untuk menghitung nilai akursi
+from sklearn import tree
 
 # def main merupakan fungsi main. Penanda titik utama program dijalankan
 def main():
-    st.title('Prediksi Diabetes dengan KNN')
+    st.title('Prediksi Diabetes Algoritma KNN')
     st.subheader('')
 
     # Membaca dataset diabetes dari file CSV dengan menggunakan fungsi read_csv dari pandas. Dataset akan disimpan dalam variabel data.
     data = pd.read_csv('diabetes.csv')
 
-    # pada baris kode ini user diminta untuk memasukkan data
-    k = st.number_input('Masukkan nilai dari K', value=5, step=1)
-    user_glucose = st.number_input('Masukkan level glukosa', value=60)
-    user_insulin = st.number_input('Masukkan level insulin', value=35)
-    user_bmi = st.number_input('Masukkan nilai BMI', value=25.0)
-    user_age = st.number_input('Masukkan umur', value=30)
+    # disini user akan menginput data. value merupakan contoh nilai default nya
+    user_pregnancies = st.number_input('Masukkan nilai kehamilan', value=1)
+    user_glucose = st.number_input('Masukkan level glukosa', value=85)
+    user_blood_pressure = st.number_input('Masukkan tekanan darah', value=66)
+    user_skin_thickness = st.number_input('Masukkan ketebalan kulit', value=29)
+    user_insulin = st.number_input('Masukkan level insulin', value=0)
+    user_bmi = st.number_input('Masukkan nilai BMI', value=26.60)
+    user_diabetes_pedigree = st.number_input('Masukkan nilai Diabetes Pedigree Function', value=0.351)
+    user_age = st.number_input('Masukkan umur', value=31)
 
-    # membuat variabel user_data yang isinya user_glucose, user_insulin, user_bmi, user_age yang akan di input oleh pengguna
-    user_data = [[user_glucose, user_insulin, user_bmi, user_age]] 
-    X = data[['Glucose', 'Insulin', 'BMI', 'Age']] # X digunakan untuk menyimpan data dari glucose, insulin, dll
-    y = data['Outcome'] # y digunakan untuk menyimpan data Outcome
+    # membuat variabel user_data yang isinya user_glucose, user_insulin, dll yang akan di input oleh pengguna
+    user_data = [[user_pregnancies, user_glucose, user_blood_pressure, user_skin_thickness, user_insulin, user_bmi, user_diabetes_pedigree, user_age]]
+    # membuat variabel X yang isinya Pregnancies dan lain lain
+    X = data[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']]
+    y = data['Outcome'] # membuat variabel y yang isinya outcome
 
     # preprocessing
-    scaler = StandardScaler()  # Membuat objek scaler dari kelas StandardScaler
-    X_scaled = scaler.fit_transform(X)  # Menyimpan data X yang telah diubah skala menggunakan scaler
-    user_data_scaled = scaler.transform(user_data)  # Mengubah skala data pengguna (user_data) menggunakan scaler yang sama
+    scaler = StandardScaler() # membuat objek scaler dari kelas StandardScaler
+    X_scaled = scaler.fit_transform(X) # Menyimpan data X yang telah diubah skala menggunakan scaler
+    user_data_scaled = scaler.transform(user_data) # mengubah skala data pengguna (user_data) menggunakan scaler yang sama
 
     # dataset di split dengan menggunakan train test split
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-    if st.button('Cek Kesehatan'): # ketika button cek kesehatan ditekan
-        st.subheader('')
+    if st.button('Cek Kesehatan'): # jika button cek kesehatan ditekan maka...
         st.subheader('')
 
-        # model untuk KNN
-        knn = KNeighborsClassifier(n_neighbors=k)  # Membuat objek KNeighborsClassifier dengan jumlah tetangga k yang ditentukan
-        knn.fit(X_train, y_train)  # Melatih model KNN menggunakan data latih X_train dan label y_train
+        knn = KNeighborsClassifier(n_neighbors=5, metric='manhattan')  # Menggunakan metrik Manhattan
+        # mengolah model menggunakan data latih X_train dan label y_train
+        # metode fit digunakan untuk melatih model dengan data yang ada.
+        knn.fit(X_train, y_train) 
 
-        # untuk memprediksi status kesehatan dari user
-        user_prediction = knn.predict(user_data_scaled)  # Memprediksi status kesehatan pengguna menggunakan data pengguna yang telah diubah skala
-
-        st.subheader('')
-        st.subheader('Hasil Prediksi') 
-        if user_prediction[0] == 1: # melakukan pengecekan. Kalau 1 berarti mendertia. Kalau 0 tidak
-            st.error('Berdasarkan dataset yang diberikan, pengguna diprediksi MENGIDAP DIABETES')
-        else:
-            st.success('Berdasarkan dataset yang diberikan, pengguna diprediksi TIDAK MENGIDAP DIABETES')
+        # KNN yang telah diolah digunakan untuk melakukan prediksi terhadap data pengguna yang telah diubah (user_data_scaled). 
+        # metode predict digunakan untuk menghasilkan prediksi berdasarkan data yang user input
+        user_prediction = knn.predict(user_data_scaled) 
 
         st.subheader('')
         st.subheader('')
-        st.subheader('Data Terdekat')
-        # menampilkan tabel yang berisi data terdekat dengan data pengguna berdasarkan metode KNN.
-        # data terdekat diambil dari data menggunakan indeks yang dihasilkan dari knn.kneighbors(user_data_scaled, return_distance=False).flatten().
-        
-        st.table(data.loc[knn.kneighbors(user_data_scaled, return_distance=False).flatten()][['Glucose', 'Insulin', 'BMI', 'Age', 'Outcome']])
-        st.subheader('')
+        st.subheader('Hasil Prediksi')
 
+        # inisialisasi untuk menghitung jumlah prediksi dengan nilai 0 (tidak mengidap diabetes) 
+        # dan nilai 1 (mengidap diabetes) pada variabel user_prediction.
+        one_count = (user_prediction == 1).sum()
+        zero_count = (user_prediction == 0).sum()
+
+        if zero_count > one_count: # jika 0 > 1 maka pengguna tidak menderita 
+            st.success('Berdasarkan dataset yang diberikan, pengguna diprediksi TIDAK MENGIDAP DIABETES dengan tingkat akurasi sebesar {:.2f}%'.format(accuracy_score(y_test, knn.predict(X_test)) * 100))
+        else: # sebaliknya menderita
+            st.error('Berdasarkan dataset yang diberikan, pengguna diprediksi MENGIDAP DIABETES dengan tingkat akurasi sebesar {:.2f}%'.format(accuracy_score(y_test, knn.predict(X_test)) * 100))
+
+        st.subheader('')
+        st.subheader('')
         st.subheader('Heatmap Prediksi Diabetes')
-        fig, ax = plt.subplots(figsize=(8, 6)) # Membuat objek gambar (fig) dan objek sumbu (ax) dengan ukuran (8, 6) dalam satuan inci.
-        # Membuat heatmap menggunakan sns.heatmap.
-        # data yang digunakan adalah data terdekat dengan data pengguna berdasarkan metode KNN.
-        # hanya kolom 'Glucose', 'Insulin', 'BMI', dan 'Age' yang digunakan.
-        # corr() digunakan untuk menghitung korelasi antara kolom-kolom tersebut.
-        # annot = True mengaktifkan anotasi nilai pada heatmap.
-        # cmap = 'coolwarm' menentukan skema warna untuk heatmap.
-        # linewidths=0.5 menentukan ketebalan garis antar sel pada heatmap.
-        # ax=ax mengarahkan heatmap untuk digambar pada sumbu yang telah dibuat sebelumnya.
-        heatmap = sns.heatmap(data.loc[knn.kneighbors(user_data_scaled, return_distance=False).flatten()][['Glucose', 'Insulin', 'BMI', 'Age']].corr(), annot=True, cmap='coolwarm', linewidths=0.5, ax=ax)
-        heatmap.xaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0.5)) # menentukan penempatan penanda sumbu x pada setiap label kolom.
-        heatmap.yaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0.5)) # menentukan penempatan penanda sumbu y pada setiap label baris.
-        plt.xticks(rotation=45) # memutar label sumbu x sebesar 45 derajat.
-        plt.yticks(rotation=0) # sumbu y tidak diputar / tetap
-        st.pyplot(fig) # Menampilkan gambar (heatmap) dalam antarmuka aplikasi Streamlit menggunakan fungsi st.pyplot.
-
-
+        fig, ax = plt.subplots(figsize=(12, 9)) # mengatur ukuran dari heatmap 
+        
+        # memilih kolom yang relevan berdasarkan hasil prediksi untuk heatmap
+        if zero_count > one_count: # data komplit ditampilkan jika pengguna tidak mengidap diabetes
+            selected_columns = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+        else: # data yang ditampilkan kurang jika pengguna mengidap diabetes
+            selected_columns = ['Glucose', 'BloodPressure', 'SkinThickness', 'BMI', 'Age']
+        
+        heatmap = sns.heatmap(data[selected_columns].corr(), annot=True, cmap='coolwarm', linewidths=0.5, ax=ax)
+        heatmap.xaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0.5))
+        heatmap.yaxis.set_major_locator(ticker.IndexLocator(base=1, offset=0.5))
+        plt.xticks(rotation=45)
+        plt.yticks(rotation=0)
+        st.pyplot(fig) # menampilkan gambar (heatmap) dalam antarmuka aplikasi Streamlit menggunakan fungsi st.pyplot.
+        
+        # Menampilkan pohon keputusan
+        st.subheader('')
+        st.subheader('')
+        st.subheader('Pohon Keputusan')
+        dt = tree.DecisionTreeClassifier()
+        dt.fit(X_train[:, [X.columns.get_loc(col) for col in selected_columns]], y_train)
+        plt.figure(figsize=(15, 9))
+        # Jika pengguna diprediksi tidak mengidap diabetes, maka semua kolom akan dipilih. 
+        # Jika pengguna diprediksi mengidap diabetes, maka hanya kolom yang relevan yang akan dipilih.
+        tree.plot_tree(dt, feature_names=selected_columns, class_names=['Tidak Diabetes', 'Diabetes'], filled=True)
+        st.pyplot(plt) # menampilkan pohon keputusan
+    
 if __name__ == '__main__':
     main()
